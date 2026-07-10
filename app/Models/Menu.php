14a2ecb7c3349d8382f5922ed2c11a9f5a82dfa8
PMMIS-CP <pmmistrader@gmail.com>
2026-07-10 23:07:03 +0330

    app\Models\Menu.php

---
+++ app/Models/Menu.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category',
+        'image',
+        'is_active',
+        'sort_order',
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+        'is_active' => 'boolean',
+        'sort_order' => 'integer',
+    ];
+}
