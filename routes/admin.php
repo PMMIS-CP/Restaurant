e9f867986dc015da10fe879a19fe8fcea47fcd57
PMMIS-CP <pmmistrader@gmail.com>
2026-07-12 21:32:36 +0330

    Add Takeout Menu Routes Admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Admin\Auth\AdminAuthenticatedSessionController;
 use App\Http\Controllers\Admin\Dashboard\DashboardController;
 use App\Http\Controllers\Admin\Dashboard\MenuManagementController;
 use App\Http\Controllers\Admin\Dashboard\MenuCategoryController;
+use App\Http\Controllers\Admin\Dashboard\MenuTakeoutCategoryController;
+use App\Http\Controllers\Admin\Dashboard\MenuTakeoutManagementController;
 
 Route::name('admin.')->group(function () {
 
@@ -41,6 +43,28 @@ Route::name('admin.')->group(function () {
             Route::get('/{menuCategory}/edit', [MenuCategoryController::class, 'edit'])->name('edit');
             Route::put('/{menuCategory}', [MenuCategoryController::class, 'update'])->name('update');
             Route::delete('/{menuCategory}', [MenuCategoryController::class, 'destroy'])->name('destroy');
+        });
+
+        // Takeout Menu Management
+        Route::prefix('takeout')->name('takeout.')->group(function () {
+            Route::get('/', [MenuTakeoutManagementController::class, 'index'])->name('index');
+            Route::get('/create', [MenuTakeoutManagementController::class, 'create'])->name('create');
+            Route::post('/', [MenuTakeoutManagementController::class, 'store'])->name('store');
+            Route::get('/{takeout}/edit', [MenuTakeoutManagementController::class, 'edit'])->name('edit');
+            Route::put('/{takeout}', [MenuTakeoutManagementController::class, 'update'])->name('update');
+            Route::delete('/{takeout}', [MenuTakeoutManagementController::class, 'destroy'])->name('destroy');
+            Route::patch('/{takeout}/toggle-active', [MenuTakeoutManagementController::class, 'toggleActive'])
+                ->name('toggle-active');
+        });
+
+        // Takeout Categories Management
+        Route::prefix('takeout-categories')->name('takeout-categories.')->group(function () {
+            Route::get('/', [MenuTakeoutCategoryController::class, 'index'])->name('index');
+            Route::get('/create', [MenuTakeoutCategoryController::class, 'create'])->name('create');
+            Route::post('/', [MenuTakeoutCategoryController::class, 'store'])->name('store');
+            Route::get('/{takeoutCategory}/edit', [MenuTakeoutCategoryController::class, 'edit'])->name('edit');
+            Route::put('/{takeoutCategory}', [MenuTakeoutCategoryController::class, 'update'])->name('update');
+            Route::delete('/{takeoutCategory}', [MenuTakeoutCategoryController::class, 'destroy'])->name('destroy');
         });
 
         Route::post('/logout', [AdminAuthenticatedSessionController::class, 'destroy'])
