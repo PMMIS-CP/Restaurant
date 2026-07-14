b094693165dc1afa8935c01f4ccb70d10052f007
PMMIS-CP <pmmistrader@gmail.com>
2026-07-14 04:33:34 +0330

    add Admin database\seeders\DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,6 +22,13 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        // Create admin user
+        Admin::create([
+            'name' => 'Admin',
+            'email' => 'admin@restaurant.test',
+            'password' => Hash::make('1234'),
         ]);
     }
 }
