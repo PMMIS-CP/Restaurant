22fad2e7ab096c04cbba7e7f49c454f76b62b367
PMMIS-CP <pmmistrader@gmail.com>
2026-07-20 07:53:13 +0330

    .

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,15 +1,15 @@
 import Alpine from 'alpinejs';
-import axios from 'axios';
+// import axios from 'axios';
 import Cropper from 'cropperjs';
 import 'cropperjs/dist/cropper.css';
 import { Image } from 'cross-image';
-import Swal from 'sweetalert2';
+// import Swal from 'sweetalert2';
 
 window.Alpine = Alpine;
-window.axios = axios;
+// window.axios = axios;
 window.Cropper = Cropper;
 window.CrossImage = { Image };
-window.Swal = Swal;
+// window.Swal = Swal;
 
 import './Amain.js';
 
